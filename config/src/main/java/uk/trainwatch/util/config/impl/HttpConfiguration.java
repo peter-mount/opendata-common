d34ecaa2fe2c57e6075f93e4539c6aeeb92745df
peter <peter@europa>
2016-03-30 08:54:23 +0100

    Fixed race condition on startup if remote server is unavailable, in this instance during startup of a docker node

--- config/src/main/java/uk/trainwatch/util/config/impl/HttpConfiguration.java
+++ config/src/main/java/uk/trainwatch/util/config/impl/HttpConfiguration.java
@@ -9,11 +9,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
+import java.net.ConnectException;
 import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.json.Json;
-import javax.json.JsonObject;
 import javax.json.JsonReader;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -33,7 +33,7 @@ public class HttpConfiguration
 
     private static final Logger LOG = Logger.getLogger( "HttpConfiguration" );
 
-    private Configuration configuration;
+    private volatile Configuration configuration;
     private final URI uri;
 
     public HttpConfiguration( URI uri )
@@ -41,43 +41,73 @@ public class HttpConfiguration
         this.uri = uri;
     }
 
+    private static final int MAX_RETRY = 5;
+    private static final long RETRY_DELAY = 500L;
+
     @Override
     protected Configuration getConfiguration()
     {
-        if( configuration == null )
-        {
+        if( configuration == null ) {
+            loadConfiguration();
+        }
+        return configuration;
+    }
+
+    private synchronized void loadConfiguration()
+    {
+        if( configuration == null ) {
             // Don't write uri as is as we may expose security details
             LOG.log( Level.INFO, () -> "Retrieving config " + uri.getScheme() + "://" + uri.getHost() + uri.getPath() );
 
-            try( CloseableHttpClient client = HttpClients.createDefault() )
-            {
-                HttpGet get = new HttpGet( uri );
-                get.setHeader( "User-Agent", "Area51 Configuration/1.0" );
-                HttpResponse response = client.execute( get );
-                switch( response.getStatusLine().getStatusCode() )
-                {
-                    case 200:
-                    case 304:
-                        try( InputStream is = response.getEntity().getContent() )
-                        {
-                            try( JsonReader r = Json.createReader( new InputStreamReader( is ) ) )
-                            {
-                                configuration = new MapConfiguration( MapBuilder.fromJsonObject( r.readObject() ).build() );
-                            }
+            try( CloseableHttpClient client = HttpClients.createDefault() ) {
+                int retry = 0;
+                do {
+                    try {
+                        if( retry > 0 ) {
+                            LOG.log( Level.INFO, "Sleeping {0}ms attempt {1}/{2}", new Object[]{RETRY_DELAY, retry, MAX_RETRY} );
+                            Thread.sleep( RETRY_DELAY );
                         }
-                        break;
-                        
-                    default:
-                        LOG.log( Level.WARNING, () -> "Error " + uri.getScheme() + "://" + uri.getHost() + uri.getPath()
-                                 + " " + response.getStatusLine().getStatusCode() );
-                        configuration = EmptyConfiguration.INSTANCE;
+                        HttpGet get = new HttpGet( uri );
+                        get.setHeader( "User-Agent", "Area51 Configuration/1.0" );
+
+                        HttpResponse response = client.execute( get );
+                        switch( response.getStatusLine().getStatusCode() ) {
+                            case 200:
+                            case 304:
+                                try( InputStream is = response.getEntity().getContent() ) {
+                                    try( JsonReader r = Json.createReader( new InputStreamReader( is ) ) ) {
+                                        configuration = new MapConfiguration( MapBuilder.fromJsonObject( r.readObject() ).build() );
+                                        return;
+                                    }
+                                }
+
+                            default:
+                                LOG.log( Level.WARNING, () -> "Error " + uri.getScheme() + "://" + uri.getHost() + uri.getPath()
+                                                              + " " + response.getStatusLine().getStatusCode() );
+                        }
+                    }
+                    catch( ConnectException ex ) {
+                        if( retry < MAX_RETRY ) {
+                            LOG.log( Level.WARNING, () -> "Failed " + ex.getMessage() );
+                        }
+                        else {
+                            throw ex;
+                        }
+                    }
+                    catch( InterruptedException ex ) {
+                        LOG.log( Level.SEVERE, null, ex );
+                    }
+
+                    retry++;
                 }
-            } catch( IOException ex )
-            {
+                while( retry < MAX_RETRY );
+            }
+            catch( IOException ex ) {
                 throw new UncheckedIOException( ex );
             }
+
+            configuration = EmptyConfiguration.INSTANCE;
         }
-        return configuration;
     }
 
 }
